40856b8dc771dbb74487356ea82dac4b538bf3e3
Anmol-Gup <sp430661@gmail.com>
2024-04-11 18:34:28 +0530

    add clicks logic in redirect component

--- src/components/RedirectUrl.jsx
+++ src/components/RedirectUrl.jsx
@@ -1,29 +1,33 @@
 import { useParams } from "react-router-dom"
-import {app} from '../firebase'
-import { collection, getFirestore, getDocs, query, where } from "firebase/firestore"
+import { app } from '../firebase'
+import { collection, getFirestore, getDocs, query, where, updateDoc, increment, onSnapshot } from "firebase/firestore"
 import { useEffect } from "react"
 
-const RedirectUrl=()=>{
-    const {shortId}=useParams()    
-    
-    useEffect(()=>{
+const RedirectUrl = () => {
+    const { shortId } = useParams()
+
+    useEffect(() => {
+        const getUrls = async () => {
+            try {
+                const db = getFirestore(app)
+                const docRef = collection(db, 'urls')
+                const q = query(docRef, where('shortId', '==', shortId))
+                const querySnapshot = await getDocs(q)
+
+                const clicks=querySnapshot.docs[0].data().clicks+1
+                await updateDoc(querySnapshot.docs[0].ref, { clicks: clicks })
+
+                window.location.replace(querySnapshot.docs[0].data().url)
+            }
+            catch (error) {
+                console.log(error.message)
+            }
+        }
+
         getUrls()
-    },[])
 
-    const getUrls=async()=>{
-        try{
-            const db=getFirestore(app)
-            const docRef=collection(db,'urls')
-            const q=query(docRef,where('shortId','==',shortId))
-            const querySnapshot=await getDocs(q)
-            window.location.replace(querySnapshot.docs[0].data().url)
-        }
-        catch(error){
-            console.log(error.message)
-        }
-    }
+    }, [shortId])
 
-
-    return(<></>)
+    return (<></>)
 }
 export default RedirectUrl
